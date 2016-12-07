700b46e0e4b2386beff018ecc07d508f00560477
kantoku009 <kantoku009@hotmail.com>
2016-12-07 22:29:36 +0900

    空白を整形

--- source/SoundInformation/CWaveFormatOperator.cpp
+++ source/SoundInformation/CWaveFormatOperator.cpp
@@ -252,7 +252,7 @@ bool CWaveFormatOperator::writeWaveFile(const string i_strFileName)
 		cerr << "CWaveFormatOperator error: do not open" << i_strFileName << endl;
 		return false;
 	}
-    
+
 	try
 	{
 		//'RIFF' write
@@ -578,7 +578,7 @@ long CWaveFormatOperator::convert4ByteDataToLong(char* i_pbyData)
 void CWaveFormatOperator::convertLongTo4ByteData(long i_lInteger, char* i_pbyData)
 {
 	bool a_bIsBigEndian = this->isBigEndian();
-	if(a_bIsBigEndian) i_lInteger = this->swapShort((char*)&i_lInteger);
+	if(a_bIsBigEndian) i_lInteger = this->swapLong((char*)&i_lInteger);
 
 	memcpy(i_pbyData, (char*)&i_lInteger, 4);
 }
